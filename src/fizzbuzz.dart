a24591db71d90ebdfc2ab2e8668eb8aa8d502b46
Solemark <larco9mason@gmail.com>
2025-01-11 19:41:50 +1000

    refactor fizzbuzz

--- src/fizzbuzz.dart
+++ src/fizzbuzz.dart
@@ -1,14 +1,31 @@
+/// Play a game of [FizzBuzz]
 class FizzBuzz {
   int fizz, buzz, max;
-  FizzBuzz(this.fizz, this.buzz, this.max);
-  String get play {
-    String output = "";
-    for (int i = 1; i < max + 1; i++) {
-      if (i % fizz == 0) output += "fizz";
-      if (i % buzz == 0) output += "buzz";
-      if (!output.endsWith("z")) output += "$i";
-      if (i < max) output += "\n";
+  FizzBuzz({required this.fizz, required this.buzz, required this.max});
+
+  /// Play [FizzBuzz] using a for loop
+  String play() {
+    String str = "";
+    for (int i = 0; i < max + 1; i++) {
+      str = this.checkConditions(i, str);
     }
-    return output;
+    return str;
+  }
+
+  /// Play [FizzBuzz] using an [Iterator]
+  String play2() {
+    return Iterable<int>.generate(this.max + 1, (i) => i).fold<String>("", (str, i) {
+      return this.checkConditions(i, str);
+    });
+  }
+
+  /// Check necessary [FizzBuzz] game conditions
+  String checkConditions(int i, String str) {
+    if (i <= 0) return "";
+    if (i % this.fizz == 0) str += "fizz";
+    if (i % this.buzz == 0) str += "buzz";
+    if (!str.endsWith('z')) str += "$i";
+    if (i < this.max) str += '\n';
+    return str;
   }
 }
--- test/fizzbuzz_test.dart
+++ test/fizzbuzz_test.dart
@@ -2,9 +2,28 @@ import 'package:test/test.dart';
 import '../src/fizzbuzz.dart';
 
 void main() {
-  test('Test fizzbuzz', () {
-    String exp = "1\n2\nfizz\n4\nbuzz\nfizz\n7\n8\nfizz\nbuzz\n11\nfizz\n13\n14\nfizzbuzz\n16\n17\nfizz\n19\nbuzz";
-    String res = FizzBuzz(3, 5, 20).play;
-    expect(exp, res);
+  late String exp;
+  late FizzBuzz fb;
+
+  setUp(() {
+    exp = "1\n2\nfizz\n4\nbuzz\nfizz\n7\n8\nfizz\nbuzz\n11\nfizz\n13\n14\nfizzbuzz\n16\n17\nfizz\n19\nbuzz";
+    fb = FizzBuzz(fizz: 3, buzz: 5, max: 20);
+  });
+
+  group("Test FizzBuzz", () {
+    test("play()", () {
+      String res = fb.play();
+      expect(exp, res);
+    });
+    test("play2()", () {
+      String res = fb.play2();
+      expect(exp, res);
+    });
+    test("checkConditions()", () {
+      String str = "1\n2\nfizz\n4\nbuzz\nfizz\n7\n8\nfizz\nbuzz\n11\nfizz\n13\n14\n";
+      String exp = "${str}fizzbuzz\n";
+      String res = fb.checkConditions(15, str);
+      expect(exp, res);
+    });
   });
 }
