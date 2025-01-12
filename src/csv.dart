d06aba12675e62f63302248e8b8a7d350f765d11
Solemark <larco9mason@gmail.com>
2025-01-12 12:05:12 +1000

    add check to make sure sure file is deleted & retry

--- src/csv.dart
+++ src/csv.dart
@@ -6,5 +6,9 @@ class CSV {
   List<List<String>> data;
   CSV(this.filename, this.data);
   Future<void> write() async => await File("${this.filename}").writeAsString(ListToCsvConverter().convert(this.data));
-  Future<void> destroy() async => await File("${this.filename}").delete();
+  Future<void> destroy([int i = 0]) async {
+    File f = File("${this.filename}");
+    await f.delete();
+    if (await f.exists() && i < 3) await this.destroy(i + 1); // if file still exists try again, max 3 attempts
+  }
 }
